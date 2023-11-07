8575dacf29f0d724724c16d9b1fa871b2d04e4da
Geer68 <germhidalgo@gmail.com>
2023-11-07 16:43:34 -0300

    fix breadCrumb.tsx

--- src/mineComponents/Breadcrumb.tsx
+++ src/mineComponents/Breadcrumb.tsx
@@ -12,11 +12,7 @@ export function Breadcrumb({
       <nav className="flex ml-9 mt-5 mb-5" aria-label="Breadcrumb">
         <ol className="inline-flex items-center space-x-1 md:space-x-3">
           <li className="inline-flex items-center">
-            <a
-              href="#"
-              className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-yellow-900 dark:text-gray-400 dark:hover:text-white"
-            >
-              <svg
+            <Link to={"/"} className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-yellow-900 dark:text-gray-400 dark:hover:text-white"><svg
                 className="w-3 h-3 mr-2.5"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
@@ -25,8 +21,7 @@ export function Breadcrumb({
               >
                 <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
               </svg>
-              Inicio
-            </a>
+              Inicio </Link>
           </li>
           <li>
             <div className="flex items-center">
@@ -45,12 +40,9 @@ export function Breadcrumb({
                   d="m1 9 4-4-4-4"
                 />
               </svg>
-              <a
-                href="#"
-                className="ml-1 text-sm font-medium text-gray-700 hover:text-yellow-900 md:ml-2 dark:text-gray-400 dark:hover:text-white"
-              >
-                {catagory}
-              </a>
+              <Link to={'/products'} className="ml-1 text-sm font-medium text-gray-700 hover:text-yellow-900 md:ml-2 dark:text-gray-400 dark:hover:text-white">
+              {catagory}
+              </Link>
             </div>
           </li>
           <li aria-current="page">
@@ -78,12 +70,5 @@ export function Breadcrumb({
         </ol>
       </nav>
     </>
-    // <section classNameName="ml-9 mt-5">
-    //   <Link to="/">Productos</Link>
-    //   <span> {'>'} </span>
-    //   <Link to="/articles">{catagory}</Link>
-    //   <span> {'>'} </span>
-    //   <span>{nombre || ''}</span>
-    // </section>
   );
 }
